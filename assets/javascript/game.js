489beacf58270028b4c83f5dc4f8c8abcabb9c55
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-04-08 21:45:47 -0500

    3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,2 @@
 var targetNumber = 53;
 var counter = 0;
-var imageCrystal = $("<img>");
-imageCrystal.addClass("crystal-image");
-imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
